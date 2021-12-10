ce1923414318d732809aa505860216fd09bf7102
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2021-12-10 10:27:29 +0000

    day 10 cleaned up part 2

--- day10/part2.py
+++ day10/part2.py
@@ -1,9 +1,6 @@
 with open('day10/input', 'r') as f:
     lines = [x for x in f.readlines()]
 
-opening = "([{<"
-closing = ")]}>"
-
 pointsMap = {")" : 1,
              "]" : 2,
              "}" : 3,
@@ -11,37 +8,27 @@ pointsMap = {")" : 1,
 
 bracketMap = {"(" : ")", "[" : "]", "{" : "}", "<" : ">"}
 
-stack = []
-
-linesToRemove = []
-for line in lines:
-    for x in [c for c in line]:
-        if x in opening:
-            stack.append(x)
-        elif x in closing:
-            el = stack.pop()
-            if el != None and x != bracketMap[el]:
-                linesToRemove.append(line)
-                break
-
-for line in linesToRemove: lines.remove(line)
-
-stack = []
 scores = []
 for line in lines:
+    ignore = False
+    stack = []
     endStr = ""
     for x in [c for c in line]:
         if x in bracketMap.keys():
             stack.append(x)
         elif x in bracketMap.values():
-            stack.pop()
-    while len(stack) > 0:
-        endStr += str(bracketMap[stack.pop()])
+            el = stack.pop()
+            if x != bracketMap[el]:
+                ignore = True
+                break
+    if not ignore:
+        while len(stack) > 0:
+            endStr += str(bracketMap[stack.pop()])
 
-    score = 0
-    for c in endStr:
-        score *= 5
-        score += pointsMap[c]
-    scores.append(score)
+        score = 0
+        for c in endStr:
+            score *= 5
+            score += pointsMap[c]
+        scores.append(score)
 
 print (sorted(scores)[int((len(scores)-1) / 2)])
